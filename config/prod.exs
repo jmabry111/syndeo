eccfc62f1c8cb9f2d035c08871c4a53f9d18e2b5
Jason Mabry <jmabry@mymabry.com>
2017-02-27 12:06:06 -0500

    fix , in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ config :connection_card, ConnectionCard.Endpoint,
 config :logger, level: :info
 
 config :connection_card, ConnectionCard.Repo,
-  adapter: Ecto.Adapters.Postgres
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
