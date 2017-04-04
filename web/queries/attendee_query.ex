895bad58d07fd412deae6a60843897b01513c98d
Jason Mabry <jmabry@mymabry.com>
2017-04-04 13:03:26 -0400

    filter admin weekly info list to current week

--- web/queries/attendee_query.ex
+++ web/queries/attendee_query.ex
@@ -1,6 +1,8 @@
 defmodule Syndeo.AttendeeQuery do
+  import Ecto.Query
   alias Syndeo.Repo
   alias Syndeo.Attendee
+  alias Syndeo.WeeklyInfo
 
   def find_attendee!(id) do
     Attendee
@@ -10,6 +12,10 @@ defmodule Syndeo.AttendeeQuery do
 
   def find_attendees_with_current_week_info do
     Repo.all(Attendee)
-    |> Repo.preload([:weekly_info])
+    |> Repo.preload(weekly_info: (from w in WeeklyInfo, where: [week_date: ^current_week()]))
+  end
+
+  defp current_week do
+    Timex.beginning_of_week(Timex.today, :sun)
   end
 end
